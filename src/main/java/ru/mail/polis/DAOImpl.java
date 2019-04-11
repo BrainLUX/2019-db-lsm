45ffe6cf8ce9fea8bc81e9ec1ea7dbcbe7de97e8
BrainLux <Veselblu@yandex.ru>
2019-04-11 17:41:50 +0300

    Stage 1 resolved

---
+++ src/main/java/ru/mail/polis/DAOImpl.java
@@ -0,0 +1,34 @@
+package ru.mail.polis;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class DAOImpl implements DAO {
+
+    private final NavigableMap<ByteBuffer, Record> base = new TreeMap<>();
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull ByteBuffer from) {
+        return base.tailMap(from).values().iterator();
+    }
+
+    @Override
+    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) {
+        base.put(key, Record.of(key, value));
+    }
+
+    @Override
+    public void remove(@NotNull ByteBuffer key) {
+        base.remove(key);
+    }
+
+    @Override
+    public void close() {
+    }
+}
