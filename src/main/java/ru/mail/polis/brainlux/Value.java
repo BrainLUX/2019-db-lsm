9969c46728932d58f6bcb6ee980b25ef5afb6f73
BrainLux <Veselblu@yandex.ru>
2019-05-08 16:44:11 +0300

    Trying to fix some errors

--- src/main/java/ru/mail/polis/brainlux/Value.java
+++ src/main/java/ru/mail/polis/brainlux/Value.java
@@ -29,7 +29,7 @@ public final class Value implements Comparable<Value> {
         if (data == null) {
             throw new IllegalArgumentException("Cell data is null");
         }
-        return data.duplicate().slice();
+        return data.asReadOnlyBuffer();
     }
 
     @Override
